ipxe/ipxe

[lacp] Add simple LACP implementation
Some switch configurations will refuse to enable our port unless we
can speak LACP to inform the switch that we are alive.  Add a very
simple passive LACP implementation that is sufficient to convince at
least Linux's bonding driver (when tested using qemu attached to a tap
device enslaved to a bond device configured as "mode=802.3ad").

Signed-off-by: Michael Brown <mcb30@ipxe.org>

src/include/ipxe/eth_slow.h
@@ -1,0 +1,255 @@
+#ifndef _IPXE_ETH_SLOW_H
+#define _IPXE_ETH_SLOW_H
+
+/** @file
+ *
+ * Ethernet slow protocols
+ *
+ */
+
+FILE_LICENCE ( GPL2_OR_LATER );
+
+/** Slow protocols header */
+struct eth_slow_header {
+	/** Slow protocols subtype */
+	uint8_t subtype;
+	/** Subtype version number */
+	uint8_t version;
+} __attribute__ (( packed ));
+
+/** LACP subtype */
+#define ETH_SLOW_SUBTYPE_LACP 1
+
+/** LACP version number */
+#define ETH_SLOW_LACP_VERSION 1
+
+/** Marker subtype */
+#define ETH_SLOW_SUBTYPE_MARKER 2
+
+/** Marker version number */
+#define ETH_SLOW_MARKER_VERSION 1
+
+/** TLV (type, length, value) header */
+struct eth_slow_tlv_header {
+	/** Type
+	 *
+	 * This is an ETH_SLOW_TLV_XXX constant.
+	 */
+	uint8_t type;
+	/** Length
+	 *
+	 * The length includes the TLV header (except for a TLV
+	 * terminator, which has a length of zero).
+	 */
+	uint8_t length;
+} __attribute__ (( packed ));
+
+/** Terminator type */
+#define ETH_SLOW_TLV_TERMINATOR 0
+
+/** Terminator length */
+#define ETH_SLOW_TLV_TERMINATOR_LEN 0
+
+/** LACP actor type */
+#define ETH_SLOW_TLV_LACP_ACTOR 1
+
+/** LACP actor length */
+#define ETH_SLOW_TLV_LACP_ACTOR_LEN \
+	( sizeof ( struct eth_slow_lacp_entity_tlv ) )
+
+/** LACP partner type */
+#define ETH_SLOW_TLV_LACP_PARTNER 2
+
+/** LACP partner length */
+#define ETH_SLOW_TLV_LACP_PARTNER_LEN \
+	( sizeof ( struct eth_slow_lacp_entity_tlv ) )
+
+/** LACP collector type */
+#define ETH_SLOW_TLV_LACP_COLLECTOR 3
+
+/** LACP collector length */
+#define ETH_SLOW_TLV_LACP_COLLECTOR_LEN \
+	( sizeof ( struct eth_slow_lacp_collector_tlv ) )
+
+/** Marker request type */
+#define ETH_SLOW_TLV_MARKER_REQUEST 1
+
+/** Marker request length */
+#define ETH_SLOW_TLV_MARKER_REQUEST_LEN \
+	( sizeof ( struct eth_slow_marker_tlv ) )
+
+/** Marker response type */
+#define ETH_SLOW_TLV_MARKER_RESPONSE 2
+
+/** Marker response length */
+#define ETH_SLOW_TLV_MARKER_RESPONSE_LEN \
+	( sizeof ( struct eth_slow_marker_tlv ) )
+
+/** Terminator TLV */
+struct eth_slow_terminator_tlv {
+	/** TLV header */
+	struct eth_slow_tlv_header tlv;
+} __attribute__ (( packed ));
+
+/** LACP entity (actor or partner) TLV */
+struct eth_slow_lacp_entity_tlv {
+	/** TLV header */
+	struct eth_slow_tlv_header tlv;
+	/** System priority
+	 *
+	 * Used to determine the order in which ports are selected for
+	 * aggregation.
+	 */
+	uint16_t system_priority;
+	/** System identifier
+	 *
+	 * Used to uniquely identify the system (i.e. the entity with
+	 * potentially multiple ports).
+	 */
+	uint8_t system[ETH_ALEN];
+	/** Key
+	 *
+	 * Used to uniquely identify a group of aggregatable ports
+	 * within a system.
+	 */
+	uint16_t key;
+	/** Port priority
+	 *
+	 * Used to determine the order in which ports are selected for
+	 * aggregation.
+	 */
+	uint16_t port_priority;
+	/** Port identifier
+	 *
+	 * Used to uniquely identify a port within a system.
+	 */
+	uint16_t port;
+	/** State
+	 *
+	 * This is the bitwise OR of zero or more LACP_STATE_XXX
+	 * constants.
+	 */
+	uint8_t state;
+	/** Reserved */
+	uint8_t reserved[3];
+} __attribute__ (( packed ));
+
+/** Maximum system priority */
+#define LACP_SYSTEM_PRIORITY_MAX 0xffff
+
+/** Maximum port priority */
+#define LACP_PORT_PRIORITY_MAX 0xff
+
+/** LACP entity is active
+ *
+ * Represented by the state character "A"/"a"
+ */
+#define LACP_STATE_ACTIVE 0x01
+
+/** LACP timeout is short
+ *
+ * Short timeout is one second, long timeout is 30s
+ *
+ * Represented by the state character "F"/"f"
+ */
+#define LACP_STATE_FAST 0x02
+
+/** LACP link is aggregateable
+ *
+ * Represented by the state characters "G"/"g"
+ */
+#define LACP_STATE_AGGREGATABLE 0x04
+
+/** LACP link is in synchronisation
+ *
+ * Represented by the state characters "S"/"s"
+ */
+#define LACP_STATE_IN_SYNC 0x08
+
+/** LACP link is collecting (receiving)
+ *
+ * Represented by the state characters "C"/"c"
+ */
+#define LACP_STATE_COLLECTING 0x10
+
+/** LACP link is distributing (transmitting)
+ *
+ * Represented by the state characters "D"/"d"
+ */
+#define LACP_STATE_DISTRIBUTING 0x20
+
+/** LACP entity is using defaulted partner information
+ *
+ * Represented by the state characters "L"/"l"
+ */
+#define LACP_STATE_DEFAULTED 0x40
+
+/** LACP entity receive state machine is in EXPIRED
+ *
+ * Represented by the state characters "X"/"x"
+ */
+#define LACP_STATE_EXPIRED 0x80
+
+/** LACP collector TLV */
+struct eth_slow_lacp_collector_tlv {
+	/** TLV header */
+	struct eth_slow_tlv_header tlv;
+	/** Maximum delay (in 10us increments) */
+	uint16_t max_delay;
+	/** Reserved */
+	uint8_t reserved[12];
+} __attribute__ (( packed ));
+
+/** Marker TLV */
+struct eth_slow_marker_tlv {
+	/** TLV header */
+	struct eth_slow_tlv_header tlv;
+	/** Requester port */
+	uint16_t port;
+	/** Requester system */
+	uint8_t system[ETH_ALEN];
+	/** Requester transaction ID */
+	uint32_t xact;
+	/** Padding */
+	uint16_t pad;
+} __attribute__ (( packed ));
+
+/** LACP packet */
+struct eth_slow_lacp {
+	/** Slow protocols header */
+	struct eth_slow_header header;
+	/** Actor information */
+	struct eth_slow_lacp_entity_tlv actor;
+	/** Partner information */
+	struct eth_slow_lacp_entity_tlv partner;
+	/** Collector information */
+	struct eth_slow_lacp_collector_tlv collector;
+	/** Terminator */
+	struct eth_slow_terminator_tlv terminator;
+	/** Reserved */
+	uint8_t reserved[50];
+} __attribute__ (( packed ));
+
+/** Marker packet */
+struct eth_slow_marker {
+	/** Slow protocols header */
+	struct eth_slow_header header;
+	/** Marker information */
+	struct eth_slow_marker_tlv marker;
+	/** Terminator */
+	struct eth_slow_terminator_tlv terminator;
+	/** Reserved */
+	uint8_t reserved[90];
+} __attribute__ (( packed ));
+
+/** Slow protocols packet */
+union eth_slow_packet {
+	/** Slow protocols header */
+	struct eth_slow_header header;
+	/** LACP packet */
+	struct eth_slow_lacp lacp;
+	/** Marker packet */
+	struct eth_slow_marker marker;
+} __attribute__ (( packed ));
+
+#endif /* _IPXE_ETH_SLOW_H */

src/net/eth_slow.c
@@ -1,0 +1,267 @@
+/*
+ * Copyright (C) 2010 Michael Brown <mbrown@fensystems.co.uk>.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+FILE_LICENCE ( GPL2_OR_LATER );
+
+#include <stdlib.h>
+#include <string.h>
+#include <byteswap.h>
+#include <errno.h>
+#include <ipxe/iobuf.h>
+#include <ipxe/netdevice.h>
+#include <ipxe/if_ether.h>
+#include <ipxe/ethernet.h>
+#include <ipxe/eth_slow.h>
+
+/** @file
+ *
+ * Ethernet slow protocols
+ *
+ * We implement a very simple passive LACP entity, that pretends that
+ * each port is the only port on an individual system.  We avoid the
+ * need for timeout logic (and retaining local state about our
+ * partner) by requesting the same timeout period (1s or 30s) as our
+ * partner requests, and then simply responding to every packet the
+ * partner sends us.
+ */
+
+struct net_protocol eth_slow_protocol;
+
+/** Slow protocols multicast address */
+static const uint8_t eth_slow_address[ETH_ALEN] =
+	{ 0x01, 0x80, 0xc2, 0x00, 0x00, 0x02 };
+
+/**
+ * Name LACP TLV type
+ *
+ * @v type		LACP TLV type
+ * @ret name		Name of LACP TLV type
+ */
+static inline __attribute__ (( always_inline )) const char *
+eth_slow_lacp_tlv_name ( uint8_t type ) {
+	switch ( type ) {
+	case ETH_SLOW_TLV_TERMINATOR:		return "terminator";
+	case ETH_SLOW_TLV_LACP_ACTOR:		return "actor";
+	case ETH_SLOW_TLV_LACP_PARTNER:		return "partner";
+	case ETH_SLOW_TLV_LACP_COLLECTOR:	return "collector";
+	default:				return "<invalid>";
+	}
+}
+
+/**
+ * Name marker TLV type
+ *
+ * @v type		Marker TLV type
+ * @ret name		Name of marker TLV type
+ */
+static inline __attribute__ (( always_inline )) const char *
+eth_slow_marker_tlv_name ( uint8_t type ) {
+	switch ( type ) {
+	case ETH_SLOW_TLV_TERMINATOR:		return "terminator";
+	case ETH_SLOW_TLV_MARKER_REQUEST:	return "request";
+	case ETH_SLOW_TLV_MARKER_RESPONSE:	return "response";
+	default:				return "<invalid>";
+	}
+}
+
+/**
+ * Name LACP state
+ *
+ * @v state		LACP state
+ * @ret name		LACP state name
+ */
+static const char * eth_slow_lacp_state_name ( uint8_t state ) {
+	static char state_chars[] = "AFGSRTLX";
+	unsigned int i;
+
+	for ( i = 0 ; i < 8 ; i++ ) {
+		state_chars[i] |= 0x20;
+		if ( state & ( 1 << i ) )
+			state_chars[i] &= ~0x20;
+	}
+	return state_chars;
+}
+
+/**
+ * Dump LACP packet
+ *
+ * @v iobuf		I/O buffer
+ * @v netdev		Network device
+ * @v label		"RX" or "TX"
+ */
+static void eth_slow_lacp_dump ( struct io_buffer *iobuf,
+				 struct net_device *netdev,
+				 const char *label ) {
+	union eth_slow_packet *eth_slow = iobuf->data;
+	struct eth_slow_lacp *lacp = &eth_slow->lacp;
+
+	DBGC ( netdev,
+	       "SLOW %p %s LACP actor (%04x,%s,%04x,%02x,%04x) [%s]\n",
+	       netdev, label, ntohs ( lacp->actor.system_priority ),
+	       eth_ntoa ( lacp->actor.system ),
+	       ntohs ( lacp->actor.key ),
+	       ntohs ( lacp->actor.port_priority ),
+	       ntohs ( lacp->actor.port ),
+	       eth_slow_lacp_state_name ( lacp->actor.state ) );
+	DBGC ( netdev,
+	       "SLOW %p %s LACP partner (%04x,%s,%04x,%02x,%04x) [%s]\n",
+	       netdev, label, ntohs ( lacp->partner.system_priority ),
+	       eth_ntoa ( lacp->partner.system ),
+	       ntohs ( lacp->partner.key ),
+	       ntohs ( lacp->partner.port_priority ),
+	       ntohs ( lacp->partner.port ),
+	       eth_slow_lacp_state_name ( lacp->partner.state ) );
+	DBGC ( netdev, "SLOW %p %s LACP collector %04x (%d us)\n",
+	       netdev, label, ntohs ( lacp->collector.max_delay ),
+	       ( ntohs ( lacp->collector.max_delay ) * 10 ) );
+	DBGC2_HDA ( netdev, 0, iobuf, iob_len ( iobuf ) );
+}
+
+/**
+ * Process incoming LACP packet
+ *
+ * @v iobuf		I/O buffer
+ * @v netdev		Network device
+ * @ret rc		Return status code
+ */
+static int eth_slow_lacp_rx ( struct io_buffer *iobuf,
+			      struct net_device *netdev ) {
+	union eth_slow_packet *eth_slow = iobuf->data;
+	struct eth_slow_lacp *lacp = &eth_slow->lacp;
+
+	eth_slow_lacp_dump ( iobuf, netdev, "RX" );
+
+	/* Build response */
+	memset ( lacp->reserved, 0, sizeof ( lacp->reserved ) );
+	memset ( &lacp->terminator, 0, sizeof ( lacp->terminator ) );
+	memset ( &lacp->collector, 0, sizeof ( lacp->collector ) );
+	lacp->collector.tlv.type = ETH_SLOW_TLV_LACP_COLLECTOR;
+	lacp->collector.tlv.length = ETH_SLOW_TLV_LACP_COLLECTOR_LEN;
+	memcpy ( &lacp->partner, &lacp->actor, sizeof ( lacp->partner ) );
+	lacp->partner.tlv.type = ETH_SLOW_TLV_LACP_PARTNER;
+	lacp->partner.tlv.length = ETH_SLOW_TLV_LACP_PARTNER_LEN;
+	memset ( &lacp->partner.reserved, 0,
+		 sizeof ( lacp->partner.reserved ) );
+	memset ( &lacp->actor, 0, sizeof ( lacp->actor ) );
+	lacp->actor.tlv.type = ETH_SLOW_TLV_LACP_ACTOR;
+	lacp->actor.tlv.length = ETH_SLOW_TLV_LACP_ACTOR_LEN;
+	lacp->actor.system_priority = htons ( LACP_SYSTEM_PRIORITY_MAX );
+	memcpy ( lacp->actor.system, netdev->ll_addr,
+		 sizeof ( lacp->actor.system ) );
+	lacp->actor.key = htons ( 1 );
+	lacp->actor.port_priority = htons ( LACP_PORT_PRIORITY_MAX );
+	lacp->actor.port = htons ( 1 );
+	lacp->actor.state = ( LACP_STATE_IN_SYNC |
+			      LACP_STATE_COLLECTING |
+			      LACP_STATE_DISTRIBUTING |
+			      ( lacp->partner.state & LACP_STATE_FAST ) );
+	lacp->header.version = ETH_SLOW_LACP_VERSION;
+
+	/* Send response */
+	eth_slow_lacp_dump ( iobuf, netdev, "TX" );
+	return net_tx ( iobuf, netdev, &eth_slow_protocol, eth_slow_address );
+}
+
+/**
+ * Dump marker packet
+ *
+ * @v iobuf		I/O buffer
+ * @v netdev		Network device
+ * @v label		"RX" or "TX"
+ */
+static void eth_slow_marker_dump ( struct io_buffer *iobuf,
+				   struct net_device *netdev,
+				   const char *label ) {
+	union eth_slow_packet *eth_slow = iobuf->data;
+	struct eth_slow_marker *marker = &eth_slow->marker;
+
+	DBGC ( netdev, "SLOW %p %s marker %s port %04x system %s xact %08x\n",
+	       netdev, label,
+	       eth_slow_marker_tlv_name ( marker->marker.tlv.type ),
+	       ntohs ( marker->marker.port ),
+	       eth_ntoa ( marker->marker.system ),
+	       ntohl ( marker->marker.xact ) );
+	DBGC2_HDA ( netdev, 0, iobuf, iob_len ( iobuf ) );
+}
+
+/**
+ * Process incoming marker packet
+ *
+ * @v iobuf		I/O buffer
+ * @v netdev		Network device
+ * @ret rc		Return status code
+ */
+static int eth_slow_marker_rx ( struct io_buffer *iobuf,
+				struct net_device *netdev ) {
+	union eth_slow_packet *eth_slow = iobuf->data;
+	struct eth_slow_marker *marker = &eth_slow->marker;
+
+	eth_slow_marker_dump ( iobuf, netdev, "RX" );
+
+	if ( marker->marker.tlv.type == ETH_SLOW_TLV_MARKER_REQUEST ) {
+		/* Send marker response */
+		marker->marker.tlv.type = ETH_SLOW_TLV_MARKER_RESPONSE;
+		eth_slow_marker_dump ( iobuf, netdev, "TX" );
+		return net_tx ( iobuf, netdev, &eth_slow_protocol,
+				eth_slow_address );
+	} else {
+		/* Discard all other marker packets */
+		free_iob ( iobuf );
+		return -EINVAL;
+	}
+}
+
+/**
+ * Process incoming slow packet
+ *
+ * @v iobuf		I/O buffer
+ * @v netdev		Network device
+ * @v ll_source		Link-layer source address
+ * @ret rc		Return status code
+ */
+static int eth_slow_rx ( struct io_buffer *iobuf,
+			 struct net_device *netdev,
+			 const void *ll_source __unused ) {
+	union eth_slow_packet *eth_slow = iobuf->data;
+
+	/* Sanity checks */
+	if ( iob_len ( iobuf ) < sizeof ( *eth_slow ) ) {
+		free_iob ( iobuf );
+		return -EINVAL;
+	}
+
+	/* Handle according to subtype */
+	switch ( eth_slow->header.subtype ) {
+	case ETH_SLOW_SUBTYPE_LACP:
+		return eth_slow_lacp_rx ( iobuf, netdev );
+	case ETH_SLOW_SUBTYPE_MARKER:
+		return eth_slow_marker_rx ( iobuf, netdev );
+	default:
+		DBGC ( netdev, "SLOW %p RX unknown subtype %02x\n",
+		       netdev, eth_slow->header.subtype );
+		free_iob ( iobuf );
+		return -EINVAL;
+	}
+}
+
+/** Slow protocol */
+struct net_protocol eth_slow_protocol __net_protocol = {
+	.name = "Slow",
+	.net_proto = htons ( ETH_P_SLOW ),
+	.rx = eth_slow_rx,
+};

etherboot/gpxe

Signed-off-by: Stefan Hajnoczi <stefanha@gmail.com>

src/include/gpxe/eth_slow.h
@@ -1,0 +1,255 @@
+#ifndef _GPXE_ETH_SLOW_H
+#define _GPXE_ETH_SLOW_H
+
+/** @file
+ *
+ * Ethernet slow protocols
+ *
+ */
+
+FILE_LICENCE ( GPL2_OR_LATER );
+
+/** Slow protocols header */
+struct eth_slow_header {
+	/** Slow protocols subtype */
+	uint8_t subtype;
+	/** Subtype version number */
+	uint8_t version;
+} __attribute__ (( packed ));
+
+/** LACP subtype */
+#define ETH_SLOW_SUBTYPE_LACP 1
+
+/** LACP version number */
+#define ETH_SLOW_LACP_VERSION 1
+
+/** Marker subtype */
+#define ETH_SLOW_SUBTYPE_MARKER 2
+
+/** Marker version number */
+#define ETH_SLOW_MARKER_VERSION 1
+
+/** TLV (type, length, value) header */
+struct eth_slow_tlv_header {
+	/** Type
+	 *
+	 * This is an ETH_SLOW_TLV_XXX constant.
+	 */
+	uint8_t type;
+	/** Length
+	 *
+	 * The length includes the TLV header (except for a TLV
+	 * terminator, which has a length of zero).
+	 */
+	uint8_t length;
+} __attribute__ (( packed ));
+
+/** Terminator type */
+#define ETH_SLOW_TLV_TERMINATOR 0
+
+/** Terminator length */
+#define ETH_SLOW_TLV_TERMINATOR_LEN 0
+
+/** LACP actor type */
+#define ETH_SLOW_TLV_LACP_ACTOR 1
+
+/** LACP actor length */
+#define ETH_SLOW_TLV_LACP_ACTOR_LEN \
+	( sizeof ( struct eth_slow_lacp_entity_tlv ) )
+
+/** LACP partner type */
+#define ETH_SLOW_TLV_LACP_PARTNER 2
+
+/** LACP partner length */
+#define ETH_SLOW_TLV_LACP_PARTNER_LEN \
+	( sizeof ( struct eth_slow_lacp_entity_tlv ) )
+
+/** LACP collector type */
+#define ETH_SLOW_TLV_LACP_COLLECTOR 3
+
+/** LACP collector length */
+#define ETH_SLOW_TLV_LACP_COLLECTOR_LEN \
+	( sizeof ( struct eth_slow_lacp_collector_tlv ) )
+
+/** Marker request type */
+#define ETH_SLOW_TLV_MARKER_REQUEST 1
+
+/** Marker request length */
+#define ETH_SLOW_TLV_MARKER_REQUEST_LEN \
+	( sizeof ( struct eth_slow_marker_tlv ) )
+
+/** Marker response type */
+#define ETH_SLOW_TLV_MARKER_RESPONSE 2
+
+/** Marker response length */
+#define ETH_SLOW_TLV_MARKER_RESPONSE_LEN \
+	( sizeof ( struct eth_slow_marker_tlv ) )
+
+/** Terminator TLV */
+struct eth_slow_terminator_tlv {
+	/** TLV header */
+	struct eth_slow_tlv_header tlv;
+} __attribute__ (( packed ));
+
+/** LACP entity (actor or partner) TLV */
+struct eth_slow_lacp_entity_tlv {
+	/** TLV header */
+	struct eth_slow_tlv_header tlv;
+	/** System priority
+	 *
+	 * Used to determine the order in which ports are selected for
+	 * aggregation.
+	 */
+	uint16_t system_priority;
+	/** System identifier
+	 *
+	 * Used to uniquely identify the system (i.e. the entity with
+	 * potentially multiple ports).
+	 */
+	uint8_t system[ETH_ALEN];
+	/** Key
+	 *
+	 * Used to uniquely identify a group of aggregatable ports
+	 * within a system.
+	 */
+	uint16_t key;
+	/** Port priority
+	 *
+	 * Used to determine the order in which ports are selected for
+	 * aggregation.
+	 */
+	uint16_t port_priority;
+	/** Port identifier
+	 *
+	 * Used to uniquely identify a port within a system.
+	 */
+	uint16_t port;
+	/** State
+	 *
+	 * This is the bitwise OR of zero or more LACP_STATE_XXX
+	 * constants.
+	 */
+	uint8_t state;
+	/** Reserved */
+	uint8_t reserved[3];
+} __attribute__ (( packed ));
+
+/** Maximum system priority */
+#define LACP_SYSTEM_PRIORITY_MAX 0xffff
+
+/** Maximum port priority */
+#define LACP_PORT_PRIORITY_MAX 0xff
+
+/** LACP entity is active
+ *
+ * Represented by the state character "A"/"a"
+ */
+#define LACP_STATE_ACTIVE 0x01
+
+/** LACP timeout is short
+ *
+ * Short timeout is one second, long timeout is 30s
+ *
+ * Represented by the state character "F"/"f"
+ */
+#define LACP_STATE_FAST 0x02
+
+/** LACP link is aggregateable
+ *
+ * Represented by the state characters "G"/"g"
+ */
+#define LACP_STATE_AGGREGATABLE 0x04
+
+/** LACP link is in synchronisation
+ *
+ * Represented by the state characters "S"/"s"
+ */
+#define LACP_STATE_IN_SYNC 0x08
+
+/** LACP link is collecting (receiving)
+ *
+ * Represented by the state characters "C"/"c"
+ */
+#define LACP_STATE_COLLECTING 0x10
+
+/** LACP link is distributing (transmitting)
+ *
+ * Represented by the state characters "D"/"d"
+ */
+#define LACP_STATE_DISTRIBUTING 0x20
+
+/** LACP entity is using defaulted partner information
+ *
+ * Represented by the state characters "L"/"l"
+ */
+#define LACP_STATE_DEFAULTED 0x40
+
+/** LACP entity receive state machine is in EXPIRED
+ *
+ * Represented by the state characters "X"/"x"
+ */
+#define LACP_STATE_EXPIRED 0x80
+
+/** LACP collector TLV */
+struct eth_slow_lacp_collector_tlv {
+	/** TLV header */
+	struct eth_slow_tlv_header tlv;
+	/** Maximum delay (in 10us increments) */
+	uint16_t max_delay;
+	/** Reserved */
+	uint8_t reserved[12];
+} __attribute__ (( packed ));
+
+/** Marker TLV */
+struct eth_slow_marker_tlv {
+	/** TLV header */
+	struct eth_slow_tlv_header tlv;
+	/** Requester port */
+	uint16_t port;
+	/** Requester system */
+	uint8_t system[ETH_ALEN];
+	/** Requester transaction ID */
+	uint32_t xact;
+	/** Padding */
+	uint16_t pad;
+} __attribute__ (( packed ));
+
+/** LACP packet */
+struct eth_slow_lacp {
+	/** Slow protocols header */
+	struct eth_slow_header header;
+	/** Actor information */
+	struct eth_slow_lacp_entity_tlv actor;
+	/** Partner information */
+	struct eth_slow_lacp_entity_tlv partner;
+	/** Collector information */
+	struct eth_slow_lacp_collector_tlv collector;
+	/** Terminator */
+	struct eth_slow_terminator_tlv terminator;
+	/** Reserved */
+	uint8_t reserved[50];
+} __attribute__ (( packed ));
+
+/** Marker packet */
+struct eth_slow_marker {
+	/** Slow protocols header */
+	struct eth_slow_header header;
+	/** Marker information */
+	struct eth_slow_marker_tlv marker;
+	/** Terminator */
+	struct eth_slow_terminator_tlv terminator;
+	/** Reserved */
+	uint8_t reserved[90];
+} __attribute__ (( packed ));
+
+/** Slow protocols packet */
+union eth_slow_packet {
+	/** Slow protocols header */
+	struct eth_slow_header header;
+	/** LACP packet */
+	struct eth_slow_lacp lacp;
+	/** Marker packet */
+	struct eth_slow_marker marker;
+} __attribute__ (( packed ));
+
+#endif /* _GPXE_ETH_SLOW_H */

src/net/eth_slow.c
@@ -1,0 +1,267 @@
+/*
+ * Copyright (C) 2010 Michael Brown <mbrown@fensystems.co.uk>.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+FILE_LICENCE ( GPL2_OR_LATER );
+
+#include <stdlib.h>
+#include <string.h>
+#include <byteswap.h>
+#include <errno.h>
+#include <gpxe/iobuf.h>
+#include <gpxe/netdevice.h>
+#include <gpxe/if_ether.h>
+#include <gpxe/ethernet.h>
+#include <gpxe/eth_slow.h>
+
+/** @file
+ *
+ * Ethernet slow protocols
+ *
+ * We implement a very simple passive LACP entity, that pretends that
+ * each port is the only port on an individual system.  We avoid the
+ * need for timeout logic (and retaining local state about our
+ * partner) by requesting the same timeout period (1s or 30s) as our
+ * partner requests, and then simply responding to every packet the
+ * partner sends us.
+ */
+
+struct net_protocol eth_slow_protocol;
+
+/** Slow protocols multicast address */
+static const uint8_t eth_slow_address[ETH_ALEN] =
+	{ 0x01, 0x80, 0xc2, 0x00, 0x00, 0x02 };
+
+/**
+ * Name LACP TLV type
+ *
+ * @v type		LACP TLV type
+ * @ret name		Name of LACP TLV type
+ */
+static inline __attribute__ (( always_inline )) const char *
+eth_slow_lacp_tlv_name ( uint8_t type ) {
+	switch ( type ) {
+	case ETH_SLOW_TLV_TERMINATOR:		return "terminator";
+	case ETH_SLOW_TLV_LACP_ACTOR:		return "actor";
+	case ETH_SLOW_TLV_LACP_PARTNER:		return "partner";
+	case ETH_SLOW_TLV_LACP_COLLECTOR:	return "collector";
+	default:				return "<invalid>";
+	}
+}
+
+/**
+ * Name marker TLV type
+ *
+ * @v type		Marker TLV type
+ * @ret name		Name of marker TLV type
+ */
+static inline __attribute__ (( always_inline )) const char *
+eth_slow_marker_tlv_name ( uint8_t type ) {
+	switch ( type ) {
+	case ETH_SLOW_TLV_TERMINATOR:		return "terminator";
+	case ETH_SLOW_TLV_MARKER_REQUEST:	return "request";
+	case ETH_SLOW_TLV_MARKER_RESPONSE:	return "response";
+	default:				return "<invalid>";
+	}
+}
+
+/**
+ * Name LACP state
+ *
+ * @v state		LACP state
+ * @ret name		LACP state name
+ */
+static const char * eth_slow_lacp_state_name ( uint8_t state ) {
+	static char state_chars[] = "AFGSRTLX";
+	unsigned int i;
+
+	for ( i = 0 ; i < 8 ; i++ ) {
+		state_chars[i] |= 0x20;
+		if ( state & ( 1 << i ) )
+			state_chars[i] &= ~0x20;
+	}
+	return state_chars;
+}
+
+/**
+ * Dump LACP packet
+ *
+ * @v iobuf		I/O buffer
+ * @v netdev		Network device
+ * @v label		"RX" or "TX"
+ */
+static void eth_slow_lacp_dump ( struct io_buffer *iobuf,
+				 struct net_device *netdev,
+				 const char *label ) {
+	union eth_slow_packet *eth_slow = iobuf->data;
+	struct eth_slow_lacp *lacp = &eth_slow->lacp;
+
+	DBGC ( netdev,
+	       "SLOW %p %s LACP actor (%04x,%s,%04x,%02x,%04x) [%s]\n",
+	       netdev, label, ntohs ( lacp->actor.system_priority ),
+	       eth_ntoa ( lacp->actor.system ),
+	       ntohs ( lacp->actor.key ),
+	       ntohs ( lacp->actor.port_priority ),
+	       ntohs ( lacp->actor.port ),
+	       eth_slow_lacp_state_name ( lacp->actor.state ) );
+	DBGC ( netdev,
+	       "SLOW %p %s LACP partner (%04x,%s,%04x,%02x,%04x) [%s]\n",
+	       netdev, label, ntohs ( lacp->partner.system_priority ),
+	       eth_ntoa ( lacp->partner.system ),
+	       ntohs ( lacp->partner.key ),
+	       ntohs ( lacp->partner.port_priority ),
+	       ntohs ( lacp->partner.port ),
+	       eth_slow_lacp_state_name ( lacp->partner.state ) );
+	DBGC ( netdev, "SLOW %p %s LACP collector %04x (%d us)\n",
+	       netdev, label, ntohs ( lacp->collector.max_delay ),
+	       ( ntohs ( lacp->collector.max_delay ) * 10 ) );
+	DBGC2_HDA ( netdev, 0, iobuf, iob_len ( iobuf ) );
+}
+
+/**
+ * Process incoming LACP packet
+ *
+ * @v iobuf		I/O buffer
+ * @v netdev		Network device
+ * @ret rc		Return status code
+ */
+static int eth_slow_lacp_rx ( struct io_buffer *iobuf,
+			      struct net_device *netdev ) {
+	union eth_slow_packet *eth_slow = iobuf->data;
+	struct eth_slow_lacp *lacp = &eth_slow->lacp;
+
+	eth_slow_lacp_dump ( iobuf, netdev, "RX" );
+
+	/* Build response */
+	memset ( lacp->reserved, 0, sizeof ( lacp->reserved ) );
+	memset ( &lacp->terminator, 0, sizeof ( lacp->terminator ) );
+	memset ( &lacp->collector, 0, sizeof ( lacp->collector ) );
+	lacp->collector.tlv.type = ETH_SLOW_TLV_LACP_COLLECTOR;
+	lacp->collector.tlv.length = ETH_SLOW_TLV_LACP_COLLECTOR_LEN;
+	memcpy ( &lacp->partner, &lacp->actor, sizeof ( lacp->partner ) );
+	lacp->partner.tlv.type = ETH_SLOW_TLV_LACP_PARTNER;
+	lacp->partner.tlv.length = ETH_SLOW_TLV_LACP_PARTNER_LEN;
+	memset ( &lacp->partner.reserved, 0,
+		 sizeof ( lacp->partner.reserved ) );
+	memset ( &lacp->actor, 0, sizeof ( lacp->actor ) );
+	lacp->actor.tlv.type = ETH_SLOW_TLV_LACP_ACTOR;
+	lacp->actor.tlv.length = ETH_SLOW_TLV_LACP_ACTOR_LEN;
+	lacp->actor.system_priority = htons ( LACP_SYSTEM_PRIORITY_MAX );
+	memcpy ( lacp->actor.system, netdev->ll_addr,
+		 sizeof ( lacp->actor.system ) );
+	lacp->actor.key = htons ( 1 );
+	lacp->actor.port_priority = htons ( LACP_PORT_PRIORITY_MAX );
+	lacp->actor.port = htons ( 1 );
+	lacp->actor.state = ( LACP_STATE_IN_SYNC |
+			      LACP_STATE_COLLECTING |
+			      LACP_STATE_DISTRIBUTING |
+			      ( lacp->partner.state & LACP_STATE_FAST ) );
+	lacp->header.version = ETH_SLOW_LACP_VERSION;
+
+	/* Send response */
+	eth_slow_lacp_dump ( iobuf, netdev, "TX" );
+	return net_tx ( iobuf, netdev, &eth_slow_protocol, eth_slow_address );
+}
+
+/**
+ * Dump marker packet
+ *
+ * @v iobuf		I/O buffer
+ * @v netdev		Network device
+ * @v label		"RX" or "TX"
+ */
+static void eth_slow_marker_dump ( struct io_buffer *iobuf,
+				   struct net_device *netdev,
+				   const char *label ) {
+	union eth_slow_packet *eth_slow = iobuf->data;
+	struct eth_slow_marker *marker = &eth_slow->marker;
+
+	DBGC ( netdev, "SLOW %p %s marker %s port %04x system %s xact %08x\n",
+	       netdev, label,
+	       eth_slow_marker_tlv_name ( marker->marker.tlv.type ),
+	       ntohs ( marker->marker.port ),
+	       eth_ntoa ( marker->marker.system ),
+	       ntohl ( marker->marker.xact ) );
+	DBGC2_HDA ( netdev, 0, iobuf, iob_len ( iobuf ) );
+}
+
+/**
+ * Process incoming marker packet
+ *
+ * @v iobuf		I/O buffer
+ * @v netdev		Network device
+ * @ret rc		Return status code
+ */
+static int eth_slow_marker_rx ( struct io_buffer *iobuf,
+				struct net_device *netdev ) {
+	union eth_slow_packet *eth_slow = iobuf->data;
+	struct eth_slow_marker *marker = &eth_slow->marker;
+
+	eth_slow_marker_dump ( iobuf, netdev, "RX" );
+
+	if ( marker->marker.tlv.type == ETH_SLOW_TLV_MARKER_REQUEST ) {
+		/* Send marker response */
+		marker->marker.tlv.type = ETH_SLOW_TLV_MARKER_RESPONSE;
+		eth_slow_marker_dump ( iobuf, netdev, "TX" );
+		return net_tx ( iobuf, netdev, &eth_slow_protocol,
+				eth_slow_address );
+	} else {
+		/* Discard all other marker packets */
+		free_iob ( iobuf );
+		return -EINVAL;
+	}
+}
+
+/**
+ * Process incoming slow packet
+ *
+ * @v iobuf		I/O buffer
+ * @v netdev		Network device
+ * @v ll_source		Link-layer source address
+ * @ret rc		Return status code
+ */
+static int eth_slow_rx ( struct io_buffer *iobuf,
+			 struct net_device *netdev,
+			 const void *ll_source __unused ) {
+	union eth_slow_packet *eth_slow = iobuf->data;
+
+	/* Sanity checks */
+	if ( iob_len ( iobuf ) < sizeof ( *eth_slow ) ) {
+		free_iob ( iobuf );
+		return -EINVAL;
+	}
+
+	/* Handle according to subtype */
+	switch ( eth_slow->header.subtype ) {
+	case ETH_SLOW_SUBTYPE_LACP:
+		return eth_slow_lacp_rx ( iobuf, netdev );
+	case ETH_SLOW_SUBTYPE_MARKER:
+		return eth_slow_marker_rx ( iobuf, netdev );
+	default:
+		DBGC ( netdev, "SLOW %p RX unknown subtype %02x\n",
+		       netdev, eth_slow->header.subtype );
+		free_iob ( iobuf );
+		return -EINVAL;
+	}
+}
+
+/** Slow protocol */
+struct net_protocol eth_slow_protocol __net_protocol = {
+	.name = "Slow",
+	.net_proto = htons ( ETH_P_SLOW ),
+	.rx = eth_slow_rx,
+};