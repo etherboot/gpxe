ipxe/ipxe

[libc] Enable automated extraction of error usage reports
Add preprocessor magic to the error definitions to enable every error
usage to be tracked.

Signed-off-by: Michael Brown <mcb30@ipxe.org>

src/hci/wireless_errors.c
@@ -20,99 +20,89 @@ FILE_LICENCE ( GPL2_OR_LATER );
 
 #include <errno.h>
 #include <ipxe/errortab.h>
+#include <ipxe/net80211_err.h>
 
 /* Record errors as though they come from the 802.11 stack */
 #undef ERRFILE
 #define ERRFILE ERRFILE_net80211
 
 /** All 802.11 errors
- *
- * These follow the 802.11 standard as much as is feasible, but most
- * have been abbreviated to fit the 50-character limit imposed by
- * strerror.
  */
 struct errortab wireless_errors[] __errortab = {
-	/* iPXE 802.11 stack errors */
-	{ EINVAL | EUNIQ_01, "Packet too short" },
-	{ EINVAL | EUNIQ_02, "Packet 802.11 version not supported" },
-	{ EINVAL | EUNIQ_03, "Packet not a data packet" },
-	{ EINVAL | EUNIQ_04, "Packet not from an Access Point" },
-	{ EINVAL | EUNIQ_05, "Packet has invalid LLC header" },
-	{ EINVAL | EUNIQ_06, "Packet decryption error", },
-	{ EINVAL | EUNIQ_07, "Invalid active scan requested" },
-
-	/* 802.11 status codes (IEEE Std 802.11-2007, Table 7-23) */
-	/* Maximum error length: 50 chars                                            | */
-	{ ECONNREFUSED | EUNIQ_01, "Unspecified failure" },
-	{ ECONNREFUSED | EUNIQ_0A, "Cannot support all requested capabilities" },
-	{ ECONNREFUSED | EUNIQ_0B, "Reassociation denied due to lack of association" },
-	{ ECONNREFUSED | EUNIQ_0C, "Association denied for another reason" },
-	{ ECONNREFUSED | EUNIQ_0D, "Authentication algorithm unsupported" },
-	{ ECONNREFUSED | EUNIQ_0E, "Authentication sequence number unexpected" },
-	{ ECONNREFUSED | EUNIQ_0F, "Authentication rejected due to challenge failure" },
-	{ ECONNREFUSED | EUNIQ_10, "Authentication rejected due to timeout" },
-	{ ECONNREFUSED | EUNIQ_11, "Association denied because AP is out of resources" },
-	{ ECONNREFUSED | EUNIQ_12, "Association denied; basic rate support required" },
-	{ ECONNREFUSED | EUNIQ_13, "Association denied; short preamble support req'd" },
-	{ ECONNREFUSED | EUNIQ_14, "Association denied; PBCC modulation support req'd" },
-	{ ECONNREFUSED | EUNIQ_15, "Association denied; Channel Agility support req'd" },
-	{ ECONNREFUSED | EUNIQ_16, "Association denied; Spectrum Management required" },
-	{ ECONNREFUSED | EUNIQ_17, "Association denied; Power Capability unacceptable" },
-	{ ECONNREFUSED | EUNIQ_18, "Association denied; Supported Channels unacceptable" },
-	{ ECONNREFUSED | EUNIQ_19, "Association denied; Short Slot Tume support req'd" },
-	{ ECONNREFUSED | EUNIQ_1A, "Association denied; DSSS-OFDM support required" },
-	{ EHOSTUNREACH,            "Unspecified, QoS-related failure" },
-	{ EHOSTUNREACH | EUNIQ_01, "Association denied; QoS AP out of QoS resources" },
-	{ EHOSTUNREACH | EUNIQ_02, "Association denied due to excessively poor link" },
-	{ EHOSTUNREACH | EUNIQ_03, "Association denied; QoS support required" },
-	{ EHOSTUNREACH | EUNIQ_05, "The request has been declined" },
-	{ EHOSTUNREACH | EUNIQ_06, "Request unsuccessful due to invalid parameters" },
-	{ EHOSTUNREACH | EUNIQ_07, "TS not created due to bad specification" },
-	{ EHOSTUNREACH | EUNIQ_08, "Invalid information element" },
-	{ EHOSTUNREACH | EUNIQ_09, "Invalid group cipher" },
-	{ EHOSTUNREACH | EUNIQ_0A, "Invalid pairwise cipher" },
-	{ EHOSTUNREACH | EUNIQ_0B, "Invalid AKMP" },
-	{ EHOSTUNREACH | EUNIQ_0C, "Unsupported RSN information element version" },
-	{ EHOSTUNREACH | EUNIQ_0D, "Invalid RSN information element capabilities" },
-	{ EHOSTUNREACH | EUNIQ_0E, "Cipher suite rejected because of security policy" },
-	{ EHOSTUNREACH | EUNIQ_0F, "TS not created due to insufficient delay" },
-	{ EHOSTUNREACH | EUNIQ_10, "Direct link is not allowed in the BSS by policy" },
-	{ EHOSTUNREACH | EUNIQ_11, "The Destination STA is not present within the BSS" },
-	{ EHOSTUNREACH | EUNIQ_12, "The Destination STA is not a QoS STA" },
-	{ EHOSTUNREACH | EUNIQ_13, "Association denied; Listen Interval is too large" },
-
-	/* 802.11 reason codes (IEEE Std 802.11-2007, Table 7-22) */
-	/* Maximum error length: 50 chars                                          | */
-	{ ECONNRESET | EUNIQ_01, "Unspecified reason" },
-	{ ECONNRESET | EUNIQ_02, "Previous authentication no longer valid" },
-	{ ECONNRESET | EUNIQ_03, "Deauthenticated due to leaving network" },
-	{ ECONNRESET | EUNIQ_04, "Disassociated due to inactivity" },
-	{ ECONNRESET | EUNIQ_05, "Disassociated because AP is out of resources" },
-	{ ECONNRESET | EUNIQ_06, "Class 2 frame received from nonauthenticated STA" },
-	{ ECONNRESET | EUNIQ_07, "Class 3 frame received from nonassociated STA" },
-	{ ECONNRESET | EUNIQ_08, "Disassociated due to roaming" },
-	{ ECONNRESET | EUNIQ_09, "STA requesting (re)association not authenticated" },
-	{ ECONNRESET | EUNIQ_0A, "Disassociated; Power Capability unacceptable" },
-	{ ECONNRESET | EUNIQ_0B, "Disassociated; Supported Channels unacceptable" },
-	{ ECONNRESET | EUNIQ_0D, "Invalid information element" },
-	{ ECONNRESET | EUNIQ_0E, "Message integrity code (MIC) failure" },
-	{ ECONNRESET | EUNIQ_0F, "4-Way Handshake timeout" },
-	{ ECONNRESET | EUNIQ_10, "Group Key Handshake timeout" },
-	{ ECONNRESET | EUNIQ_11, "4-Way Handshake information element changed unduly" },
-	{ ECONNRESET | EUNIQ_12, "Invalid group cipher" },
-	{ ECONNRESET | EUNIQ_13, "Invalid pairwise cipher" },
-	{ ECONNRESET | EUNIQ_14, "Invalid AKMP" },
-	{ ECONNRESET | EUNIQ_15, "Unsupported RSN information element version" },
-	{ ECONNRESET | EUNIQ_16, "Invalid RSN information element capabilities" },
-	{ ECONNRESET | EUNIQ_17, "IEEE 802.1X authentication failed" },
-	{ ECONNRESET | EUNIQ_18, "Cipher suite rejected because of security policy" },
-	{ ENETRESET,            "Disassociated for unspecified, QoS-related reason" },
-	{ ENETRESET | EUNIQ_01, "Disassociated; QoS AP is out of QoS resources" },
-	{ ENETRESET | EUNIQ_02, "Disassociated due to excessively poor link" },
-	{ ENETRESET | EUNIQ_03, "Disassociated due to TXOP limit violation" },
-	{ ENETRESET | EUNIQ_04, "Requested; STA is leaving the BSS (or resetting)" },
-	{ ENETRESET | EUNIQ_05, "Requested; does not want to use the mechanism" },
-	{ ENETRESET | EUNIQ_06, "Requested; setup is required" },
-	{ ENETRESET | EUNIQ_07, "Requested from peer STA due to timeout" },
-	{ ENETRESET | EUNIQ_0D, "Peer STA does not support requested cipher suite" },
+	__einfo_errortab ( EINFO_EINVAL_PKT_TOO_SHORT ),
+	__einfo_errortab ( EINFO_EINVAL_PKT_VERSION ),
+	__einfo_errortab ( EINFO_EINVAL_PKT_NOT_DATA ),
+	__einfo_errortab ( EINFO_EINVAL_PKT_NOT_FROMDS ),
+	__einfo_errortab ( EINFO_EINVAL_PKT_LLC_HEADER ),
+	__einfo_errortab ( EINFO_EINVAL_CRYPTO_REQUEST ),
+	__einfo_errortab ( EINFO_EINVAL_ACTIVE_SCAN ),
+	__einfo_errortab ( EINFO_ECONNREFUSED_FAILURE ),
+	__einfo_errortab ( EINFO_ECONNREFUSED_CAPAB_UNSUPP ),
+	__einfo_errortab ( EINFO_ECONNREFUSED_REASSOC_INVALID ),
+	__einfo_errortab ( EINFO_ECONNREFUSED_ASSOC_DENIED ),
+	__einfo_errortab ( EINFO_ECONNREFUSED_AUTH_ALGO_UNSUPP ),
+	__einfo_errortab ( EINFO_ECONNREFUSED_AUTH_SEQ_INVALID ),
+	__einfo_errortab ( EINFO_ECONNREFUSED_AUTH_CHALL_INVALID ),
+	__einfo_errortab ( EINFO_ECONNREFUSED_AUTH_TIMEOUT ),
+	__einfo_errortab ( EINFO_ECONNREFUSED_ASSOC_NO_ROOM ),
+	__einfo_errortab ( EINFO_ECONNREFUSED_ASSOC_NEED_RATE ),
+	__einfo_errortab ( EINFO_ECONNREFUSED_ASSOC_NEED_SHORT_PMBL ),
+	__einfo_errortab ( EINFO_ECONNREFUSED_ASSOC_NEED_PBCC ),
+	__einfo_errortab ( EINFO_ECONNREFUSED_ASSOC_NEED_CHAN_AGILITY ),
+	__einfo_errortab ( EINFO_ECONNREFUSED_ASSOC_NEED_SPECTRUM_MGMT ),
+	__einfo_errortab ( EINFO_ECONNREFUSED_ASSOC_BAD_POWER ),
+	__einfo_errortab ( EINFO_ECONNREFUSED_ASSOC_BAD_CHANNELS ),
+	__einfo_errortab ( EINFO_ECONNREFUSED_ASSOC_NEED_SHORT_SLOT ),
+	__einfo_errortab ( EINFO_ECONNREFUSED_ASSOC_NEED_DSSS_OFDM ),
+	__einfo_errortab ( EINFO_EHOSTUNREACH_QOS_FAILURE ),
+	__einfo_errortab ( EINFO_EHOSTUNREACH_QOS_NO_ROOM ),
+	__einfo_errortab ( EINFO_EHOSTUNREACH_LINK_IS_HORRIBLE ),
+	__einfo_errortab ( EINFO_EHOSTUNREACH_ASSOC_NEED_QOS ),
+	__einfo_errortab ( EINFO_EHOSTUNREACH_REQUEST_DECLINED ),
+	__einfo_errortab ( EINFO_EHOSTUNREACH_REQUEST_INVALID ),
+	__einfo_errortab ( EINFO_EHOSTUNREACH_TS_NOT_CREATED_AGAIN ),
+	__einfo_errortab ( EINFO_EHOSTUNREACH_INVALID_IE ),
+	__einfo_errortab ( EINFO_EHOSTUNREACH_GROUP_CIPHER_INVALID ),
+	__einfo_errortab ( EINFO_EHOSTUNREACH_PAIR_CIPHER_INVALID ),
+	__einfo_errortab ( EINFO_EHOSTUNREACH_AKMP_INVALID ),
+	__einfo_errortab ( EINFO_EHOSTUNREACH_RSN_VERSION_UNSUPP ),
+	__einfo_errortab ( EINFO_EHOSTUNREACH_RSN_CAPAB_INVALID ),
+	__einfo_errortab ( EINFO_EHOSTUNREACH_CIPHER_REJECTED ),
+	__einfo_errortab ( EINFO_EHOSTUNREACH_TS_NOT_CREATED_WAIT ),
+	__einfo_errortab ( EINFO_EHOSTUNREACH_DIRECT_LINK_FORBIDDEN ),
+	__einfo_errortab ( EINFO_EHOSTUNREACH_DEST_NOT_PRESENT ),
+	__einfo_errortab ( EINFO_EHOSTUNREACH_DEST_NOT_QOS ),
+	__einfo_errortab ( EINFO_EHOSTUNREACH_ASSOC_LISTEN_TOO_HIGH ),
+	__einfo_errortab ( EINFO_ECONNRESET_UNSPECIFIED ),
+	__einfo_errortab ( EINFO_ECONNRESET_AUTH_NO_LONGER_VALID ),
+	__einfo_errortab ( EINFO_ECONNRESET_LEAVING ),
+	__einfo_errortab ( EINFO_ECONNRESET_INACTIVITY ),
+	__einfo_errortab ( EINFO_ECONNRESET_OUT_OF_RESOURCES ),
+	__einfo_errortab ( EINFO_ECONNRESET_NEED_AUTH ),
+	__einfo_errortab ( EINFO_ECONNRESET_NEED_ASSOC ),
+	__einfo_errortab ( EINFO_ECONNRESET_LEAVING_TO_ROAM ),
+	__einfo_errortab ( EINFO_ECONNRESET_REASSOC_INVALID ),
+	__einfo_errortab ( EINFO_ECONNRESET_BAD_POWER ),
+	__einfo_errortab ( EINFO_ECONNRESET_BAD_CHANNELS ),
+	__einfo_errortab ( EINFO_ECONNRESET_INVALID_IE ),
+	__einfo_errortab ( EINFO_ECONNRESET_MIC_FAILURE ),
+	__einfo_errortab ( EINFO_ECONNRESET_4WAY_TIMEOUT ),
+	__einfo_errortab ( EINFO_ECONNRESET_GROUPKEY_TIMEOUT ),
+	__einfo_errortab ( EINFO_ECONNRESET_4WAY_INVALID ),
+	__einfo_errortab ( EINFO_ECONNRESET_GROUP_CIPHER_INVALID ),
+	__einfo_errortab ( EINFO_ECONNRESET_PAIR_CIPHER_INVALID ),
+	__einfo_errortab ( EINFO_ECONNRESET_AKMP_INVALID ),
+	__einfo_errortab ( EINFO_ECONNRESET_RSN_VERSION_INVALID ),
+	__einfo_errortab ( EINFO_ECONNRESET_RSN_CAPAB_INVALID ),
+	__einfo_errortab ( EINFO_ECONNRESET_8021X_FAILURE ),
+	__einfo_errortab ( EINFO_ECONNRESET_CIPHER_REJECTED ),
+	__einfo_errortab ( EINFO_ENETRESET_QOS_UNSPECIFIED ),
+	__einfo_errortab ( EINFO_ENETRESET_QOS_OUT_OF_RESOURCES ),
+	__einfo_errortab ( EINFO_ENETRESET_LINK_IS_HORRIBLE ),
+	__einfo_errortab ( EINFO_ENETRESET_INVALID_TXOP ),
+	__einfo_errortab ( EINFO_ENETRESET_REQUESTED_LEAVING ),
+	__einfo_errortab ( EINFO_ENETRESET_REQUESTED_NO_USE ),
+	__einfo_errortab ( EINFO_ENETRESET_REQUESTED_NEED_SETUP ),
+	__einfo_errortab ( EINFO_ENETRESET_REQUESTED_TIMEOUT ),
+	__einfo_errortab ( EINFO_ENETRESET_CIPHER_UNSUPPORTED ),
 };

src/net/80211/net80211.c
@@ -23,6 +23,8 @@ FILE_LICENCE ( GPL2_OR_LATER );
 #include <string.h>
 #include <byteswap.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
 #include <ipxe/settings.h>
 #include <ipxe/if_arp.h>
 #include <ipxe/ethernet.h>
@@ -32,51 +34,13 @@ FILE_LICENCE ( GPL2_OR_LATER );
 #include <ipxe/sec80211.h>
 #include <ipxe/timer.h>
 #include <ipxe/nap.h>
-#include <unistd.h>
-#include <errno.h>
+#include <ipxe/errortab.h>
+#include <ipxe/net80211_err.h>
 
 /** @file
  *
  * 802.11 device management
  */
-
-/* Disambiguate the EINVAL's a bit */
-#define EINVAL_PKT_TOO_SHORT	( EINVAL | EUNIQ_01 )
-#define EINVAL_PKT_VERSION	( EINVAL | EUNIQ_02 )
-#define EINVAL_PKT_NOT_DATA	( EINVAL | EUNIQ_03 )
-#define EINVAL_PKT_NOT_FROMDS	( EINVAL | EUNIQ_04 )
-#define EINVAL_PKT_LLC_HEADER	( EINVAL | EUNIQ_05 )
-#define EINVAL_CRYPTO_REQUEST	( EINVAL | EUNIQ_06 )
-#define EINVAL_ACTIVE_SCAN	( EINVAL | EUNIQ_07 )
-
-/*
- * 802.11 error codes: The AP can give us a status code explaining why
- * authentication failed, or a reason code explaining why we were
- * deauthenticated/disassociated. These codes range from 0-63 (the
- * field is 16 bits wide, but only up to 45 or so are defined yet; we
- * allow up to 63 for extensibility). This is encoded into an error
- * code as such:
- *
- *                                      status & 0x1f goes here --vv--
- *   Status code 0-31:  ECONNREFUSED | EUNIQ_(status & 0x1f) (0e1a6038)
- *   Status code 32-63: EHOSTUNREACH | EUNIQ_(status & 0x1f) (171a6011)
- *   Reason code 0-31:  ECONNRESET | EUNIQ_(reason & 0x1f)   (0f1a6039)
- *   Reason code 32-63: ENETRESET | EUNIQ_(reason & 0x1f)    (271a6001)
- *
- * The POSIX error codes more or less convey the appropriate message
- * (status codes occur when we can't associate at all, reason codes
- * when we lose association unexpectedly) and let us extract the
- * complete 802.11 error code from the rc value.
- */
-
-/** Make return status code from 802.11 status code */
-#define E80211_STATUS( stat )  ( ((stat & 0x20)? EHOSTUNREACH : ECONNREFUSED) \
-					| ((stat & 0x1f) << 8) )
-
-/** Make return status code from 802.11 reason code */
-#define E80211_REASON( reas )  ( ((reas & 0x20)? ENETRESET : ECONNRESET) \
-					| ((reas & 0x1f) << 8) )
-
 
 /** List of 802.11 devices */
 static struct list_head net80211_devices = LIST_HEAD_INIT ( net80211_devices );
@@ -2838,3 +2802,15 @@ void net80211_tx_complete ( struct net80211_device *dev,
 	/* Pass completion onward */
 	netdev_tx_complete_err ( dev->netdev, iob, rc );
 }
+
+/** Common 802.11 errors */
+struct errortab common_wireless_errors[] __errortab = {
+	__einfo_errortab ( EINFO_EINVAL_CRYPTO_REQUEST ),
+	__einfo_errortab ( EINFO_ECONNRESET_UNSPECIFIED ),
+	__einfo_errortab ( EINFO_ECONNRESET_INACTIVITY ),
+	__einfo_errortab ( EINFO_ECONNRESET_4WAY_TIMEOUT ),
+	__einfo_errortab ( EINFO_ECONNRESET_8021X_FAILURE ),
+	__einfo_errortab ( EINFO_ECONNREFUSED_FAILURE ),
+	__einfo_errortab ( EINFO_ECONNREFUSED_ASSOC_DENIED ),
+	__einfo_errortab ( EINFO_ECONNREFUSED_AUTH_ALGO_UNSUPP ),
+};

etherboot/gpxe

Signed-off-by: Stefan Hajnoczi <stefanha@gmail.com>

src/hci/wireless_errors.c
@@ -20,99 +20,89 @@ FILE_LICENCE ( GPL2_OR_LATER );
 
 #include <errno.h>
 #include <gpxe/errortab.h>
+#include <gpxe/net80211_err.h>
 
 /* Record errors as though they come from the 802.11 stack */
 #undef ERRFILE
 #define ERRFILE ERRFILE_net80211
 
 /** All 802.11 errors
- *
- * These follow the 802.11 standard as much as is feasible, but most
- * have been abbreviated to fit the 50-character limit imposed by
- * strerror.
  */
 struct errortab wireless_errors[] __errortab = {
-	/* gPXE 802.11 stack errors */
-	{ EINVAL | EUNIQ_01, "Packet too short" },
-	{ EINVAL | EUNIQ_02, "Packet 802.11 version not supported" },
-	{ EINVAL | EUNIQ_03, "Packet not a data packet" },
-	{ EINVAL | EUNIQ_04, "Packet not from an Access Point" },
-	{ EINVAL | EUNIQ_05, "Packet has invalid LLC header" },
-	{ EINVAL | EUNIQ_06, "Packet decryption error", },
-	{ EINVAL | EUNIQ_07, "Invalid active scan requested" },
-
-	/* 802.11 status codes (IEEE Std 802.11-2007, Table 7-23) */
-	/* Maximum error length: 50 chars                                            | */
-	{ ECONNREFUSED | EUNIQ_01, "Unspecified failure" },
-	{ ECONNREFUSED | EUNIQ_0A, "Cannot support all requested capabilities" },
-	{ ECONNREFUSED | EUNIQ_0B, "Reassociation denied due to lack of association" },
-	{ ECONNREFUSED | EUNIQ_0C, "Association denied for another reason" },
-	{ ECONNREFUSED | EUNIQ_0D, "Authentication algorithm unsupported" },
-	{ ECONNREFUSED | EUNIQ_0E, "Authentication sequence number unexpected" },
-	{ ECONNREFUSED | EUNIQ_0F, "Authentication rejected due to challenge failure" },
-	{ ECONNREFUSED | EUNIQ_10, "Authentication rejected due to timeout" },
-	{ ECONNREFUSED | EUNIQ_11, "Association denied because AP is out of resources" },
-	{ ECONNREFUSED | EUNIQ_12, "Association denied; basic rate support required" },
-	{ ECONNREFUSED | EUNIQ_13, "Association denied; short preamble support req'd" },
-	{ ECONNREFUSED | EUNIQ_14, "Association denied; PBCC modulation support req'd" },
-	{ ECONNREFUSED | EUNIQ_15, "Association denied; Channel Agility support req'd" },
-	{ ECONNREFUSED | EUNIQ_16, "Association denied; Spectrum Management required" },
-	{ ECONNREFUSED | EUNIQ_17, "Association denied; Power Capability unacceptable" },
-	{ ECONNREFUSED | EUNIQ_18, "Association denied; Supported Channels unacceptable" },
-	{ ECONNREFUSED | EUNIQ_19, "Association denied; Short Slot Tume support req'd" },
-	{ ECONNREFUSED | EUNIQ_1A, "Association denied; DSSS-OFDM support required" },
-	{ EHOSTUNREACH,            "Unspecified, QoS-related failure" },
-	{ EHOSTUNREACH | EUNIQ_01, "Association denied; QoS AP out of QoS resources" },
-	{ EHOSTUNREACH | EUNIQ_02, "Association denied due to excessively poor link" },
-	{ EHOSTUNREACH | EUNIQ_03, "Association denied; QoS support required" },
-	{ EHOSTUNREACH | EUNIQ_05, "The request has been declined" },
-	{ EHOSTUNREACH | EUNIQ_06, "Request unsuccessful due to invalid parameters" },
-	{ EHOSTUNREACH | EUNIQ_07, "TS not created due to bad specification" },
-	{ EHOSTUNREACH | EUNIQ_08, "Invalid information element" },
-	{ EHOSTUNREACH | EUNIQ_09, "Invalid group cipher" },
-	{ EHOSTUNREACH | EUNIQ_0A, "Invalid pairwise cipher" },
-	{ EHOSTUNREACH | EUNIQ_0B, "Invalid AKMP" },
-	{ EHOSTUNREACH | EUNIQ_0C, "Unsupported RSN information element version" },
-	{ EHOSTUNREACH | EUNIQ_0D, "Invalid RSN information element capabilities" },
-	{ EHOSTUNREACH | EUNIQ_0E, "Cipher suite rejected because of security policy" },
-	{ EHOSTUNREACH | EUNIQ_0F, "TS not created due to insufficient delay" },
-	{ EHOSTUNREACH | EUNIQ_10, "Direct link is not allowed in the BSS by policy" },
-	{ EHOSTUNREACH | EUNIQ_11, "The Destination STA is not present within the BSS" },
-	{ EHOSTUNREACH | EUNIQ_12, "The Destination STA is not a QoS STA" },
-	{ EHOSTUNREACH | EUNIQ_13, "Association denied; Listen Interval is too large" },
-
-	/* 802.11 reason codes (IEEE Std 802.11-2007, Table 7-22) */
-	/* Maximum error length: 50 chars                                          | */
-	{ ECONNRESET | EUNIQ_01, "Unspecified reason" },
-	{ ECONNRESET | EUNIQ_02, "Previous authentication no longer valid" },
-	{ ECONNRESET | EUNIQ_03, "Deauthenticated due to leaving network" },
-	{ ECONNRESET | EUNIQ_04, "Disassociated due to inactivity" },
-	{ ECONNRESET | EUNIQ_05, "Disassociated because AP is out of resources" },
-	{ ECONNRESET | EUNIQ_06, "Class 2 frame received from nonauthenticated STA" },
-	{ ECONNRESET | EUNIQ_07, "Class 3 frame received from nonassociated STA" },
-	{ ECONNRESET | EUNIQ_08, "Disassociated due to roaming" },
-	{ ECONNRESET | EUNIQ_09, "STA requesting (re)association not authenticated" },
-	{ ECONNRESET | EUNIQ_0A, "Disassociated; Power Capability unacceptable" },
-	{ ECONNRESET | EUNIQ_0B, "Disassociated; Supported Channels unacceptable" },
-	{ ECONNRESET | EUNIQ_0D, "Invalid information element" },
-	{ ECONNRESET | EUNIQ_0E, "Message integrity code (MIC) failure" },
-	{ ECONNRESET | EUNIQ_0F, "4-Way Handshake timeout" },
-	{ ECONNRESET | EUNIQ_10, "Group Key Handshake timeout" },
-	{ ECONNRESET | EUNIQ_11, "4-Way Handshake information element changed unduly" },
-	{ ECONNRESET | EUNIQ_12, "Invalid group cipher" },
-	{ ECONNRESET | EUNIQ_13, "Invalid pairwise cipher" },
-	{ ECONNRESET | EUNIQ_14, "Invalid AKMP" },
-	{ ECONNRESET | EUNIQ_15, "Unsupported RSN information element version" },
-	{ ECONNRESET | EUNIQ_16, "Invalid RSN information element capabilities" },
-	{ ECONNRESET | EUNIQ_17, "IEEE 802.1X authentication failed" },
-	{ ECONNRESET | EUNIQ_18, "Cipher suite rejected because of security policy" },
-	{ ENETRESET,            "Disassociated for unspecified, QoS-related reason" },
-	{ ENETRESET | EUNIQ_01, "Disassociated; QoS AP is out of QoS resources" },
-	{ ENETRESET | EUNIQ_02, "Disassociated due to excessively poor link" },
-	{ ENETRESET | EUNIQ_03, "Disassociated due to TXOP limit violation" },
-	{ ENETRESET | EUNIQ_04, "Requested; STA is leaving the BSS (or resetting)" },
-	{ ENETRESET | EUNIQ_05, "Requested; does not want to use the mechanism" },
-	{ ENETRESET | EUNIQ_06, "Requested; setup is required" },
-	{ ENETRESET | EUNIQ_07, "Requested from peer STA due to timeout" },
-	{ ENETRESET | EUNIQ_0D, "Peer STA does not support requested cipher suite" },
+	__einfo_errortab ( EINFO_EINVAL_PKT_TOO_SHORT ),
+	__einfo_errortab ( EINFO_EINVAL_PKT_VERSION ),
+	__einfo_errortab ( EINFO_EINVAL_PKT_NOT_DATA ),
+	__einfo_errortab ( EINFO_EINVAL_PKT_NOT_FROMDS ),
+	__einfo_errortab ( EINFO_EINVAL_PKT_LLC_HEADER ),
+	__einfo_errortab ( EINFO_EINVAL_CRYPTO_REQUEST ),
+	__einfo_errortab ( EINFO_EINVAL_ACTIVE_SCAN ),
+	__einfo_errortab ( EINFO_ECONNREFUSED_FAILURE ),
+	__einfo_errortab ( EINFO_ECONNREFUSED_CAPAB_UNSUPP ),
+	__einfo_errortab ( EINFO_ECONNREFUSED_REASSOC_INVALID ),
+	__einfo_errortab ( EINFO_ECONNREFUSED_ASSOC_DENIED ),
+	__einfo_errortab ( EINFO_ECONNREFUSED_AUTH_ALGO_UNSUPP ),
+	__einfo_errortab ( EINFO_ECONNREFUSED_AUTH_SEQ_INVALID ),
+	__einfo_errortab ( EINFO_ECONNREFUSED_AUTH_CHALL_INVALID ),
+	__einfo_errortab ( EINFO_ECONNREFUSED_AUTH_TIMEOUT ),
+	__einfo_errortab ( EINFO_ECONNREFUSED_ASSOC_NO_ROOM ),
+	__einfo_errortab ( EINFO_ECONNREFUSED_ASSOC_NEED_RATE ),
+	__einfo_errortab ( EINFO_ECONNREFUSED_ASSOC_NEED_SHORT_PMBL ),
+	__einfo_errortab ( EINFO_ECONNREFUSED_ASSOC_NEED_PBCC ),
+	__einfo_errortab ( EINFO_ECONNREFUSED_ASSOC_NEED_CHAN_AGILITY ),
+	__einfo_errortab ( EINFO_ECONNREFUSED_ASSOC_NEED_SPECTRUM_MGMT ),
+	__einfo_errortab ( EINFO_ECONNREFUSED_ASSOC_BAD_POWER ),
+	__einfo_errortab ( EINFO_ECONNREFUSED_ASSOC_BAD_CHANNELS ),
+	__einfo_errortab ( EINFO_ECONNREFUSED_ASSOC_NEED_SHORT_SLOT ),
+	__einfo_errortab ( EINFO_ECONNREFUSED_ASSOC_NEED_DSSS_OFDM ),
+	__einfo_errortab ( EINFO_EHOSTUNREACH_QOS_FAILURE ),
+	__einfo_errortab ( EINFO_EHOSTUNREACH_QOS_NO_ROOM ),
+	__einfo_errortab ( EINFO_EHOSTUNREACH_LINK_IS_HORRIBLE ),
+	__einfo_errortab ( EINFO_EHOSTUNREACH_ASSOC_NEED_QOS ),
+	__einfo_errortab ( EINFO_EHOSTUNREACH_REQUEST_DECLINED ),
+	__einfo_errortab ( EINFO_EHOSTUNREACH_REQUEST_INVALID ),
+	__einfo_errortab ( EINFO_EHOSTUNREACH_TS_NOT_CREATED_AGAIN ),
+	__einfo_errortab ( EINFO_EHOSTUNREACH_INVALID_IE ),
+	__einfo_errortab ( EINFO_EHOSTUNREACH_GROUP_CIPHER_INVALID ),
+	__einfo_errortab ( EINFO_EHOSTUNREACH_PAIR_CIPHER_INVALID ),
+	__einfo_errortab ( EINFO_EHOSTUNREACH_AKMP_INVALID ),
+	__einfo_errortab ( EINFO_EHOSTUNREACH_RSN_VERSION_UNSUPP ),
+	__einfo_errortab ( EINFO_EHOSTUNREACH_RSN_CAPAB_INVALID ),
+	__einfo_errortab ( EINFO_EHOSTUNREACH_CIPHER_REJECTED ),
+	__einfo_errortab ( EINFO_EHOSTUNREACH_TS_NOT_CREATED_WAIT ),
+	__einfo_errortab ( EINFO_EHOSTUNREACH_DIRECT_LINK_FORBIDDEN ),
+	__einfo_errortab ( EINFO_EHOSTUNREACH_DEST_NOT_PRESENT ),
+	__einfo_errortab ( EINFO_EHOSTUNREACH_DEST_NOT_QOS ),
+	__einfo_errortab ( EINFO_EHOSTUNREACH_ASSOC_LISTEN_TOO_HIGH ),
+	__einfo_errortab ( EINFO_ECONNRESET_UNSPECIFIED ),
+	__einfo_errortab ( EINFO_ECONNRESET_AUTH_NO_LONGER_VALID ),
+	__einfo_errortab ( EINFO_ECONNRESET_LEAVING ),
+	__einfo_errortab ( EINFO_ECONNRESET_INACTIVITY ),
+	__einfo_errortab ( EINFO_ECONNRESET_OUT_OF_RESOURCES ),
+	__einfo_errortab ( EINFO_ECONNRESET_NEED_AUTH ),
+	__einfo_errortab ( EINFO_ECONNRESET_NEED_ASSOC ),
+	__einfo_errortab ( EINFO_ECONNRESET_LEAVING_TO_ROAM ),
+	__einfo_errortab ( EINFO_ECONNRESET_REASSOC_INVALID ),
+	__einfo_errortab ( EINFO_ECONNRESET_BAD_POWER ),
+	__einfo_errortab ( EINFO_ECONNRESET_BAD_CHANNELS ),
+	__einfo_errortab ( EINFO_ECONNRESET_INVALID_IE ),
+	__einfo_errortab ( EINFO_ECONNRESET_MIC_FAILURE ),
+	__einfo_errortab ( EINFO_ECONNRESET_4WAY_TIMEOUT ),
+	__einfo_errortab ( EINFO_ECONNRESET_GROUPKEY_TIMEOUT ),
+	__einfo_errortab ( EINFO_ECONNRESET_4WAY_INVALID ),
+	__einfo_errortab ( EINFO_ECONNRESET_GROUP_CIPHER_INVALID ),
+	__einfo_errortab ( EINFO_ECONNRESET_PAIR_CIPHER_INVALID ),
+	__einfo_errortab ( EINFO_ECONNRESET_AKMP_INVALID ),
+	__einfo_errortab ( EINFO_ECONNRESET_RSN_VERSION_INVALID ),
+	__einfo_errortab ( EINFO_ECONNRESET_RSN_CAPAB_INVALID ),
+	__einfo_errortab ( EINFO_ECONNRESET_8021X_FAILURE ),
+	__einfo_errortab ( EINFO_ECONNRESET_CIPHER_REJECTED ),
+	__einfo_errortab ( EINFO_ENETRESET_QOS_UNSPECIFIED ),
+	__einfo_errortab ( EINFO_ENETRESET_QOS_OUT_OF_RESOURCES ),
+	__einfo_errortab ( EINFO_ENETRESET_LINK_IS_HORRIBLE ),
+	__einfo_errortab ( EINFO_ENETRESET_INVALID_TXOP ),
+	__einfo_errortab ( EINFO_ENETRESET_REQUESTED_LEAVING ),
+	__einfo_errortab ( EINFO_ENETRESET_REQUESTED_NO_USE ),
+	__einfo_errortab ( EINFO_ENETRESET_REQUESTED_NEED_SETUP ),
+	__einfo_errortab ( EINFO_ENETRESET_REQUESTED_TIMEOUT ),
+	__einfo_errortab ( EINFO_ENETRESET_CIPHER_UNSUPPORTED ),
 };

src/net/80211/net80211.c
@@ -23,6 +23,8 @@ FILE_LICENCE ( GPL2_OR_LATER );
 #include <string.h>
 #include <byteswap.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
 #include <gpxe/settings.h>
 #include <gpxe/if_arp.h>
 #include <gpxe/ethernet.h>
@@ -32,51 +34,13 @@ FILE_LICENCE ( GPL2_OR_LATER );
 #include <gpxe/sec80211.h>
 #include <gpxe/timer.h>
 #include <gpxe/nap.h>
-#include <unistd.h>
-#include <errno.h>
+#include <gpxe/errortab.h>
+#include <gpxe/net80211_err.h>
 
 /** @file
  *
  * 802.11 device management
  */
-
-/* Disambiguate the EINVAL's a bit */
-#define EINVAL_PKT_TOO_SHORT	( EINVAL | EUNIQ_01 )
-#define EINVAL_PKT_VERSION	( EINVAL | EUNIQ_02 )
-#define EINVAL_PKT_NOT_DATA	( EINVAL | EUNIQ_03 )
-#define EINVAL_PKT_NOT_FROMDS	( EINVAL | EUNIQ_04 )
-#define EINVAL_PKT_LLC_HEADER	( EINVAL | EUNIQ_05 )
-#define EINVAL_CRYPTO_REQUEST	( EINVAL | EUNIQ_06 )
-#define EINVAL_ACTIVE_SCAN	( EINVAL | EUNIQ_07 )
-
-/*
- * 802.11 error codes: The AP can give us a status code explaining why
- * authentication failed, or a reason code explaining why we were
- * deauthenticated/disassociated. These codes range from 0-63 (the
- * field is 16 bits wide, but only up to 45 or so are defined yet; we
- * allow up to 63 for extensibility). This is encoded into an error
- * code as such:
- *
- *                                      status & 0x1f goes here --vv--
- *   Status code 0-31:  ECONNREFUSED | EUNIQ_(status & 0x1f) (0e1a6038)
- *   Status code 32-63: EHOSTUNREACH | EUNIQ_(status & 0x1f) (171a6011)
- *   Reason code 0-31:  ECONNRESET | EUNIQ_(reason & 0x1f)   (0f1a6039)
- *   Reason code 32-63: ENETRESET | EUNIQ_(reason & 0x1f)    (271a6001)
- *
- * The POSIX error codes more or less convey the appropriate message
- * (status codes occur when we can't associate at all, reason codes
- * when we lose association unexpectedly) and let us extract the
- * complete 802.11 error code from the rc value.
- */
-
-/** Make return status code from 802.11 status code */
-#define E80211_STATUS( stat )  ( ((stat & 0x20)? EHOSTUNREACH : ECONNREFUSED) \
-					| ((stat & 0x1f) << 8) )
-
-/** Make return status code from 802.11 reason code */
-#define E80211_REASON( reas )  ( ((reas & 0x20)? ENETRESET : ECONNRESET) \
-					| ((reas & 0x1f) << 8) )
-
 
 /** List of 802.11 devices */
 static struct list_head net80211_devices = LIST_HEAD_INIT ( net80211_devices );
@@ -2838,3 +2802,15 @@ void net80211_tx_complete ( struct net80211_device *dev,
 	/* Pass completion onward */
 	netdev_tx_complete_err ( dev->netdev, iob, rc );
 }
+
+/** Common 802.11 errors */
+struct errortab common_wireless_errors[] __errortab = {
+	__einfo_errortab ( EINFO_EINVAL_CRYPTO_REQUEST ),
+	__einfo_errortab ( EINFO_ECONNRESET_UNSPECIFIED ),
+	__einfo_errortab ( EINFO_ECONNRESET_INACTIVITY ),
+	__einfo_errortab ( EINFO_ECONNRESET_4WAY_TIMEOUT ),
+	__einfo_errortab ( EINFO_ECONNRESET_8021X_FAILURE ),
+	__einfo_errortab ( EINFO_ECONNREFUSED_FAILURE ),
+	__einfo_errortab ( EINFO_ECONNREFUSED_ASSOC_DENIED ),
+	__einfo_errortab ( EINFO_ECONNREFUSED_AUTH_ALGO_UNSUPP ),
+};