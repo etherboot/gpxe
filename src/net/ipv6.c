d70f33b85b9b2fd1f469296238d18494b57b4535
Matthew Iselin <matthew@theiselins.net>
2011-06-27 19:02:27 +1000

    [ipv6] Clean up inet6_ntoa
    Signed-off-by: Matthew Iselin <matthew@theiselins.net>
    Signed-off-by: Marty Connor <mdc@etherboot.org>

--- src/net/ipv6.c
+++ src/net/ipv6.c
@@ -542,18 +542,12 @@ char * inet6_ntoa ( struct in6_addr in6 ) {
 int inet6_aton ( const char *cp, struct in6_addr *inp ) {
 	char convbuf[40];
 	char *tmp = convbuf, *next = convbuf;
-	size_t i = 0, len = strlen ( cp );
+	size_t i = 0;
 	int ok;
 	char c;
 	
 	/* Verify a valid address. */
-	if ( ! len ) {
-		return 0;
-	}
-	
-	for ( ; i < len; i++ ) {
-		c = cp[i];
-		
+	while ( ( c = cp[i++] ) ) {
 		ok = c == ':';
 		ok = ok || ( ( c >= '0' ) && ( c <= '9' ) );
 		ok = ok || ( ( c >= 'a' ) && ( c <= 'f' ) );
@@ -562,6 +556,9 @@ int inet6_aton ( const char *cp, struct in6_addr *inp ) {
 		if ( ! ok ) {
 			return 0;
 		}
+	}
+	if ( ! i ) {
+		return 0;
 	}
 	
 	strcpy ( convbuf, cp );
